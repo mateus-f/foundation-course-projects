05678ccc7cd37959e482bc649348eb997b5fd0ce
Mateus Ferreira <mateusjunior803@gmail.com>
2025-08-09 20:27:44 -0300

    :sparkles: feat: Add Rock Paper Scissors project with UI
    Upload the Rock Paper Scissors game including its complete user
    interface implementation.

--- desafio-03/script.js
+++ desafio-03/script.js
@@ -1,75 +1,147 @@
-const ROUNDS = 5;
+const rounds = document.querySelector(".round > span");
+let remainingRounds = 5;
+
+rounds.textContent = addZero(remainingRounds);
 
 function getComputerChoice() {
-  const choices = ['rock', 'paper', 'scissors'];
-  const randomChoice = choices[Math.floor(Math.random() * choices.length)];
+	const CHOICES = ["rock", "paper", "scissors"];
 
-  return randomChoice;
+	const randomChoice = CHOICES[Math.floor(Math.random() * CHOICES.length)];
+	return randomChoice;
 }
 
-function getHumanChoice() {
-  const selectedValue = prompt(
-    `Choose a value:
-    1. Rock
-    2. Paper
-    3. Scissors`
-  );
+function getHumanChoice(selectedValue) {
+	return selectedValue;
+}
 
-  const choices = {
-    1: 'rock',
-    2: 'paper',
-    3: 'scissors'
-  };
+function showResultCard(computerScore, humanScore) {
+	const USER_INFORMATION = {
+		player: {
+			picture: "src/assets/img/player-picture.webp",
+			name: "You",
+			category: "Player",
+			quote: "Guess I’ve got more skill<br>in me than I thought!",
+		},
+		machine: {
+			picture: "src/assets/img/machine-picture.webp",
+			name: "Lenny No-Loss",
+			category: "Machine",
+			quote: "Another one for the collection.<br>Losing? Never heard of it!",
+		},
+	};
 
-  return choices[selectedValue];
+	const overlay = document.querySelector(".overlay");
+	const resultCard = document.querySelector(".result-card");
+	const h2 = resultCard.querySelector("h2");
+	const contentContainer = resultCard.querySelector(".user-content");
+	const newGameButton = resultCard.querySelector("button");
+
+	const userContainer = {
+		picture: contentContainer.querySelector("img"),
+		name: contentContainer.querySelector("div .name"),
+		category: contentContainer.querySelector("div .category"),
+		quote: contentContainer.querySelector("q"),
+	};
+
+	const result =
+		computerScore > humanScore
+			? "machine"
+			: humanScore > computerScore
+			? "player"
+			: "draw";
+
+	overlay.setAttribute("aria-hidden", "false");
+	overlay.classList.toggle("show");
+
+	if (result !== "draw") {
+		h2.textContent = "THE WINNER IS...";
+
+		if (result === "player") {
+			userContainer.picture.setAttribute(
+				"src",
+				USER_INFORMATION.player.picture
+			);
+			userContainer.picture.classList.add("player");
+			userContainer.name.textContent = USER_INFORMATION.player.name;
+			userContainer.category.textContent = USER_INFORMATION.player.category;
+			userContainer.quote.innerHTML = USER_INFORMATION.player.quote;
+		} else {
+			userContainer.picture.setAttribute(
+				"src",
+				USER_INFORMATION.machine.picture
+			);
+			userContainer.picture.classList.add("machine");
+			userContainer.name.textContent = USER_INFORMATION.machine.name;
+			userContainer.category.textContent = USER_INFORMATION.machine.category;
+			userContainer.quote.innerHTML = USER_INFORMATION.machine.quote;
+		}
+	} else {
+		h2.textContent = "DRAW! :/";
+		contentContainer.style.display = "none";
+	}
+
+	newGameButton.addEventListener("click", () => window.location.reload());
+}
+
+function addZero(element) {
+	return element.toString().padStart(2, "0");
 }
 
 function playGame() {
-  const selection = {
-    computer: '',
-    human: ''
-  }
+	const buttons = document.querySelectorAll(".moves > button");
 
-  let score = {
-    human: 0,
-    computer: 0
-  };
+	let score = {
+		human: 0,
+		computer: 0,
+	};
 
-  function playRound(computerChoice, humanChoice) {
-    const rules = {
-      rock: 'scissors',
-      scissors: 'paper',
-      paper: 'rock'
-    };
+	buttons.forEach((button) =>
+		button.addEventListener("click", () => {
+			if (remainingRounds !== 0) {
+				playRound(getComputerChoice(), getHumanChoice(button.value));
+			} else {
+				buttons.disabled = true;
+				showResultCard(score.computer, score.human);
+			}
+		})
+	);
 
-    let message = "";
+	function playRound(computerChoice, humanChoice) {
+		const RULES = {
+			rock: "scissors",
+			scissors: "paper",
+			paper: "rock",
+		};
+		const SELECTED_EMOTE = {
+			rock: "✊",
+			scissors: "✌️",
+			paper: "👋",
+		};
 
-    if (humanChoice === computerChoice) {
-      message = 'Draw!';
-    } else if (humanChoice === rules[computerChoice]) {
-      message = `Computer won! ${computerChoice} beats ${humanChoice}.`;
-      score.computer++;
-    } else {
-      message = `Human won! ${humanChoice} beats ${computerChoice}.`;
-      score.human++;
-    }
+		const scores = document.querySelectorAll(".score > span");
+		const emotes = document.querySelectorAll(".emote");
+		const textBottom = document.querySelector(".select-move > p");
 
-    console.group('%cROUND', 'color: #fcfcfcff; background-color: #323233ff; padding: 5px; font-weight: bold;')
-    console.log(message);
-    console.log(`Computer: ${score.computer} | Human: ${score.human}`);
-    console.groupEnd()
-  }
+		let message = textBottom.textContent;
 
-  console.log('%cTHE GAME HAS BEGUN!!!', 'color: #dcdc00; background-color: #404000ff; padding: 5px; font-weight: bold;');
+		if (humanChoice === computerChoice) {
+			message = "Draw!";
+		} else if (humanChoice === RULES[computerChoice]) {
+			message = `Lenny No-Loss won! ${computerChoice} beats ${humanChoice}.`;
+			score.computer++;
+		} else {
+			message = `You won! ${humanChoice} beats ${computerChoice}.`;
+			score.human++;
+		}
 
-  for (let i = 1; i <= ROUNDS; i++) {
-    selection.computer = getComputerChoice();
-    selection.human = getHumanChoice();
-    playRound(selection.computer, selection.human);
-  }
-
-  console.log('%cTHE GAME IS OVER!!!', 'color: #dc0000ff; background-color: #400000ff; padding: 5px; font-weight: bold;');
-  console.log(`%cTHE FINAL WINNER IS... ${selection.computer > selection.human ? 'COMPUTER' : 'HUMAN'}! CONGRATULATIONS!`, 'color: #21dc00ff; background-color: #004003ff; padding: 5px; font-weight: bold;');
+		remainingRounds--;
+		scores[0].textContent = addZero(score.computer);
+		scores[1].textContent = addZero(score.human);
+		rounds.textContent = addZero(remainingRounds);
+		emotes[0].textContent = SELECTED_EMOTE[computerChoice];
+		emotes[1].textContent = SELECTED_EMOTE[humanChoice];
+		textBottom.textContent = message;
+	}
 }
 
 playGame();
